128997629843bab5441b67f816e1d79512ae7ae7
TVTelugu <63017421+tvtelugu@users.noreply.github.com>
2024-04-22 14:01:15 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,1 @@
+function streamMatch(e){document.getElementById("videoPlayer").innerHTML="";var t=document.createElement("iframe");t.src=e,t.setAttribute("allowfullscreen","true"),t.width="100%",t.height="600",document.getElementById("videoPlayer").appendChild(t)}
