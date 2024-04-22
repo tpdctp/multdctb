6210058af257c472db875949b1cc4f4f77609dc1
TVTelugu <63017421+tvtelugu@users.noreply.github.com>
2024-04-22 14:07:17 +0530

    Delete style.js

--- style.js
+++
@@ -1,1 +0,0 @@
-function streamMatch(e){document.getElementById("videoPlayer").innerHTML="";var t=document.createElement("iframe");t.src=e,t.setAttribute("allowfullscreen","true"),t.width="100%",t.height="600",document.getElementById("videoPlayer").appendChild(t)}
